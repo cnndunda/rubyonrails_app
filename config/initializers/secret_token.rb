9a1b9c8b432ba830f85f31985cc18c1ed5ebc3fc
Gisty LLC <gistyllc@gmail.com>
2013-12-28 03:39:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyonrailsApp::Application.config.secret_key_base = 'c78384cb79205ee48ad637140327a1393e8958d83b09fd7147373c7b8b145f01fa3767badc32b7843f1050e8bce9a9af8c9b64d42042b8188dfe4e8d58535e75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyonrailsApp::Application.config.session_store :cookie_store, key: '_rubyonrails_app_session'
